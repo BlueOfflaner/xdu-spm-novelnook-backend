9f0889b22ebb7a9b2fb78117b357302fb6ec6ed3
violet <2114568838@qq.com>
2023-04-04 19:04:00 +0800

    init-v1.2

---
+++ nook-gateway/src/main/java/com/xdu/gateway/GatewayApplication.java
@@ -0,0 +1,9 @@
+package com.xdu.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+public class GatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

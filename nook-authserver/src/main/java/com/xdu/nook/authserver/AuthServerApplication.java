e99424b8d2de13e64b59e5cc7c1102c4d4a673ff
violet <2114568838@qq.com>
2023-04-05 10:22:42 +0800

    init-v1.5.

--- nook-authserver/src/main/java/com/xdu/nook/authserver/AuthServerApplication.java
+++ nook-authserver/src/main/java/com/xdu/nook/authserver/AuthServerApplication.java
@@ -1,7 +1,15 @@
 package com.xdu.nook.authserver;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@EnableAspectJAutoProxy
+@EnableFeignClients
+@EnableDiscoveryClient
 public class AuthServerApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        SpringApplication.run(AuthServerApplication.class, args);
     }
 }
